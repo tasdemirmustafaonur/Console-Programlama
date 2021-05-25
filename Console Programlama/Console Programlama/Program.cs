edc30372e3004229a4c31d69e2e0aedd760ab77e
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-05-25 16:45:11 +0300

    Add project files.

---
+++ Console Programlama/Console Programlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
