143ee48d3c5fd83994597a92a41ca885771ea924
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-06-11 01:42:22 +0300

    Project1

---
+++ Console Programlama/PhoneBook/PersonList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TelefonRehberUygulaması
+{
+    public static class PersonList
+    {
+        public static List<Person> persons;
+
+
+        static PersonList()
+        {
+            persons = SetPersons();
+        }
+
+
+        public static List<Person> SetPersons()
+        {
+            return new List<Person>()
+            {
+                new Person("Yağız","Ünal","5456301745"),
+                new Person("Özgür","Taşdemir","5357051520"),
+                new Person("Fatih","Taşdemir","5354781514"),
+                new Person("Mustafa","Taşdemir","5468532021"),
+                new Person("Oktay","Ağca","5473302527")
+            };
+        }
+    }
+}
