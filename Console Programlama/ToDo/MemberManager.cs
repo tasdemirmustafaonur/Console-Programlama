c4a2f3954932317993c452b8eeefc524cf9b062a
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-06-11 02:03:11 +0300

    Project2

---
+++ Console Programlama/ToDo/MemberManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public static class MemberManager
+    {
+        public static List<Member> Members;
+
+        static MemberManager()
+        {
+            Members = DefaultMembers();
+        }
+
+        public static List<Member> DefaultMembers()
+        {
+            return new List<Member>(){
+            new Member(1,"Yağız Ünal"),
+            new Member(2,"Özgür Taşdemir"),
+            new Member(3,"Fatih Taşdemir"),
+            new Member(4,"Mustafa Taşdemir"),
+            new Member(5,"OKtay Ağca"),
+            };
+        }
+    }
+}
